aeb6ec9848e42df8b39830e789399f61d093a077
programming-person <harryfielding17@gmail.com>
2018-05-14 21:15:33 +0100

    Added AR item

--- src/main/java/com/harry/items/Items.java
+++ src/main/java/com/harry/items/Items.java
@@ -3,5 +3,5 @@ package com.harry.items;
 import net.minecraft.item.Item;
 
 public class Items {
-    public static final Item[] ITEMS = new Item[]{new TestItem("TestItem")};
+    public static final Item[] ITEMS = new Item[]{new TestItem("TestItem"), new AR("AssaultRifle")};
 }
