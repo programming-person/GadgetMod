0a17f292e15473eea5ee9b21190b9247f53c6903
programming-person <harryfielding17@gmail.com>
2018-05-14 21:14:46 +0100

    Added EntityBullet class

---
+++ src/main/java/com/harry/entity/EntityBullet.java
@@ -0,0 +1,25 @@
+package com.harry.entity;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.IThrowableEntity;
+
+public class EntityBullet extends EntityThrowable {
+    public EntityBullet(World world) {
+        super(world);
+    }
+    public EntityBullet(World world, EntityLivingBase entity) {
+        super(world, entity);
+
+    }
+    @Override
+    public void onImpact(RayTraceResult result) {
+        if (result.entityHit == null) {
+            return;
+        }
+        result.entityHit.attackEntityFrom(DamageSource.GENERIC, 1.0F);
+    }
+}
